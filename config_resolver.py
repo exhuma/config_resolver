ef154d0f88b566e26d02c4fb48d5df8b8a36bbec
Michel Albert <michel@albert.lu>
2012-11-26 15:39:22 +0100

    Preventing conflicts if more packages use this module.

--- config_resolver.py
+++ config_resolver.py
@@ -15,11 +15,10 @@ from os import getenv, pathsep, getcwd
 from os.path import expanduser, exists, join
 import logging
 
-__version__ = '2.0.2'
+__version__ = '2.1'
 
 LOG = logging.getLogger(__name__)
-CONF = SafeConfigParser()
-CONFIG_LOADED = False
+__CONFIGURATIONS = {}
 
 
 def config(group, app, search_path=None, conf_name=None, force_reload=False):
@@ -27,8 +26,9 @@ def config(group, app, search_path=None, conf_name=None, force_reload=False):
     Searches for an appropriate config file. If found, return the parsed
     config instance.
 
-    After this is called, the config instance will also be available as
-    ``config_resolver.CONF`` as a shortcut.
+    Calling this function multiple times is okay as the configurations are
+    internally cached in a dictionary. If you want to reload the
+    configuration, simply pass ``force_reload`` as ``True``
 
     :param group: an application group (f. ex.: your company name)
     :param app: an application identifier (f.ex.: the application module name)
@@ -42,7 +42,7 @@ def config(group, app, search_path=None, conf_name=None, force_reload=False):
     :param conf_name: if specified, this can be used to override the
         configuration filename (default=``"app.ini"``)
     :param force_reload: if set to true, the config is reloaded, even if it
-        was alrady loaded in the application. Default = ``False``
+        was already loaded in the application. Default = ``False``
 
     Environment Variables
     ---------------------
@@ -58,13 +58,14 @@ def config(group, app, search_path=None, conf_name=None, force_reload=False):
     <app>_CONFIG
         The file name of the config file (default=``"app.ini"``)
     """
-    global CONFIG_LOADED
+
+    groupdict = __CONFIGURATIONS.setdefault(group, {})
 
     # only load the config if necessary (or explicitly requested)
-    if CONFIG_LOADED and not force_reload:
+    if app in groupdict and not force_reload:
         LOG.debug('Returning cached config instance. Use '
                 '``force_reload=True`` to avoid caching!')
-        return CONF
+        return groupdict[app]
 
     path_var = "%s_PATH" % app.upper()
     filename_var = "%s_CONFIG" % app.upper()
@@ -93,20 +94,20 @@ def config(group, app, search_path=None, conf_name=None, force_reload=False):
 
     # Next, use the resolved path to find the filenames. Keep track of which
     # files we loaded in order to inform the user.
-    CONFIG_LOADED = False
+    conf = SafeConfigParser()
     for dirname in path:
         conf_name = join(dirname, config_filename)
         if exists(conf_name):
-            CONF.read(conf_name)
-            LOG.info('{0} config from {1}'.format(
-                CONFIG_LOADED and 'Updating' or 'Loading initial',
+            conf.read(conf_name)
+            LOG.info('%s config from %s' % (
+                app in groupdict and 'Updating' or 'Loading initial',
                 conf_name))
-            CONFIG_LOADED = True
+            groupdict[app] = conf
         else:
-            LOG.debug('{0} does not exist. Skipping...'.format(conf_name))
+            LOG.debug('%s does not exist. Skipping...' % (conf_name, ))
 
-    if not CONFIG_LOADED:
+    if app not in groupdict:
         LOG.warning("No config file named %s found! Search path was %r" % (
             config_filename, path))
 
-    return CONF
+    return conf
