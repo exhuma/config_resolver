896a8bf07b3a3a8f5bde86d1383a5599b91efeb8
Michel Albert <michel@albert.lu>
2012-12-11 07:51:37 +0100

    Pushed everythin into a class.
    Having the configuration stored in a module variable makes it more difficult
    to handle multiple configurations (from different modules).
    
    By putting everything into a class, an API user may create as many instances
    as he/she wants.

--- config_resolver.py
+++ config_resolver.py
@@ -15,99 +15,116 @@ from os import getenv, pathsep, getcwd
 from os.path import expanduser, exists, join
 import logging
 
-__version__ = '2.1'
+__version__ = '3.0'
 
 LOG = logging.getLogger(__name__)
-__CONFIGURATIONS = {}
 
 
-def config(group, app, search_path=None, conf_name=None, force_reload=False):
-    """
-    Searches for an appropriate config file. If found, return the parsed
-    config instance.
+class Config(object):
 
-    Calling this function multiple times is okay as the configurations are
-    internally cached in a dictionary. If you want to reload the
-    configuration, simply pass ``force_reload`` as ``True``
+    def __init__(self, group_name, app_name):
+        self.config = None
+        self.load(group_name, app_name)
 
-    :param group: an application group (f. ex.: your company name)
-    :param app: an application identifier (f.ex.: the application module name)
-    :param search_path: if specified, set the config search path to the given
-        value. The path can use OS specific separators (f.ex.: ``:`` on posix,
-        ``;`` on windows) to specify multiple folders. These folders will be
-        searched in the specified order. The config files will be loaded
-        incrementally. This means that the each subsequent config file will
-        extend/override existing values. This means that the last file will
-        take precedence.
-    :param conf_name: if specified, this can be used to override the
-        configuration filename (default=``"app.ini"``)
-    :param force_reload: if set to true, the config is reloaded, even if it
-        was already loaded in the application. Default = ``False``
+    def load(self, group_name, app_name, search_path=None,
+            file_name=None, reload=False):
+        """
+        Searches for an appropriate config file. If found, loads the file into
+        the current :py:class:`Config` instance. This method can also be used
+        to re-load a configuration. Note that you may want to set ``reload``
+        to ``True`` to clear the configuration before loading in that case.
+        Without doing that, values will remain available even if they have
+        been removed from the config files.
 
-    Environment Variables
-    ---------------------
+        :param group_name: an application group (f. ex.: your company name)
+        :param app_name: an application identifier (f.ex.: the application
+                         module name)
+        :param search_path: if specified, set the config search path to the
+            given value. The path can use OS specific separators (f.ex.: ``:``
+            on posix, ``;`` on windows) to specify multiple folders. These
+            folders will be searched in the specified order. The config files
+            will be loaded incrementally. This means that the each subsequent
+            config file will extend/override existing values. This means that
+            the last file will take precedence.
+        :param file_name: if specified, this can be used to override the
+            configuration filename (default=``"app.ini"``)
+        :param reload: if set to ``True``, the existing values are cleared
+                       before reloading.
 
-    The resolver can also be manipulated using environment variables to allow
-    different values for different running instances:
+        Environment Variables
+        ---------------------
 
-    <app>_PATH
-        The search path of config files. ``<app>`` is the application name in
-        all caps. See the documentation for the ``search_path`` parameter for
-        an explanation of precedence.
+        The resolver can also be manipulated using environment variables to
+        allow different values for different running instances:
 
-    <app>_CONFIG
-        The file name of the config file (default=``"app.ini"``)
-    """
+        <app_name>_PATH
+            The search path of config files. ``<app_name>`` is the application
+            name in all caps. See the documentation for the ``search_path``
+            parameter for an explanation of precedence.
 
-    groupdict = __CONFIGURATIONS.setdefault(group, {})
+        <app_name>_CONFIG
+            The file name of the config file (default=``"app.ini"``)
+        """
 
-    # only load the config if necessary (or explicitly requested)
-    if app in groupdict and not force_reload:
-        LOG.debug('Returning cached config instance. Use '
-                '``force_reload=True`` to avoid caching!')
-        return groupdict[app]
+        if reload:
+            self.config = None
 
-    path_var = "%s_PATH" % app.upper()
-    filename_var = "%s_CONFIG" % app.upper()
+        # only load the config if necessary (or explicitly requested)
+        if self.config:
+            LOG.debug('Returning cached config instance. Use '
+                    '``reload=True`` to avoid caching!')
+            return self.config
 
-    # default search path
-    path = ['/etc/%s/%s' % (group, app),
-            expanduser('~/.%s/%s' % (group, app)),
-            getcwd(),]
+        path_var = "%s_PATH" % app_name.upper()
+        filename_var = "%s_CONFIG" % app_name.upper()
 
-    # if an environment variable was specified, override the default path
-    env_path = getenv(path_var)
-    if env_path:
-        path = env_path.split(pathsep)
+        # default search path
+        path = ['/etc/%s/%s' % (group_name, app_name),
+                expanduser('~/.%s/%s' % (group_name, app_name)),
+                getcwd()]
 
-    # If a path was passed directly to this method, override the path again
-    if search_path:
-        path = search_path.split(pathsep)
+        # If a path was passed directly to this method, override the path.
+        if search_path:
+            path = search_path.split(pathsep)
 
-    # same logic for the configuration filename. First, try the runtime
-    # environment (with a default fallback):
-    config_filename = getenv(filename_var, 'app.ini')
+        # if an environment variable was specified, override the path again.
+        # Environment variables take absolute precedence.
+        env_path = getenv(path_var)
+        if env_path:
+            LOG.info('Configuration search path was overridden with {0} by an '
+                     'environment vaiable.'.format(env_path))
+            path = env_path.split(pathsep)
 
-    # If a filename was passed directly, override the value
-    if conf_name:
-        config_filename = conf_name
-
-    # Next, use the resolved path to find the filenames. Keep track of which
-    # files we loaded in order to inform the user.
-    conf = SafeConfigParser()
-    for dirname in path:
-        conf_name = join(dirname, config_filename)
-        if exists(conf_name):
-            conf.read(conf_name)
-            LOG.info('%s config from %s' % (
-                app in groupdict and 'Updating' or 'Loading initial',
-                conf_name))
-            groupdict[app] = conf
+        # same logic for the configuration filename. First, check the method
+        # argument...
+        if file_name:
+            config_filename = file_name
         else:
-            LOG.debug('%s does not exist. Skipping...' % (conf_name, ))
+            config_filename = None
 
-    if app not in groupdict:
-        LOG.warning("No config file named %s found! Search path was %r" % (
-            config_filename, path))
+        # ... next, take the value from the environment
+        env_filename = getenv(filename_var)
+        if env_filename:
+            LOG.info('Configuration filename was overridden with {0} by an '
+                     'environment vaiable.'.format(env_filename))
+            config_filename = env_filename
 
-    return conf
+        # Next, use the resolved path to find the filenames. Keep track of
+        # which files we loaded in order to inform the user.
+        conf = SafeConfigParser()
+        for dirname in path:
+            conf_name = join(dirname, config_filename)
+            if exists(conf_name):
+                conf.read(conf_name)
+                LOG.info('%s config from %s' % (
+                    self.config and 'Updating' or 'Loading initial',
+                    conf_name))
+                self.config = conf
+            else:
+                LOG.debug('%s does not exist. Skipping...' % (conf_name, ))
+
+        if not self.conf:
+            LOG.warning("No config file named %s found! Search path was %r" % (
+                config_filename, path))
+
+        return conf
