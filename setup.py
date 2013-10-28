2687f8054ab2e083123574042702c6b8933b8041
Michel Albert <michel@albert.lu>
2013-10-28 10:40:03 +0100

    Added TROVE classiiers.

--- setup.py
+++ setup.py
@@ -18,4 +18,17 @@ setup(
     license="LGPL",
     include_package_data=True,
     py_modules=['config_resolver'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 '
+        '(LGPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
