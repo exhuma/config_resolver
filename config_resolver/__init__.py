exhuma/config_resolver

commit 54168cd8ad6a2c9543ace5da4802804169d5d652

Move config_resolver into a package

config_resolver/__init__.py
@@ -1,0 +1,3 @@
+# pylint: skip-file
+from .core import Config, SecuredConfig, __version__, get_config
+from .exc import NoVersionError

test.py
@@ -450,7 +450,7 @@ class FunctionalityTests(TestBase):
         NOTE: This is a *user* warning. Not a developer warning! So we'll use
         the logging module instead of the warnings module!
         """
-        with patch('config_resolver.Config.check_file') as checker_mock:
+        with patch('config_resolver.core.Config.check_file') as checker_mock:
             checker_mock.return_value = (True, "")
             Config('hello', 'world')
             expected_message = (
@@ -520,7 +520,7 @@ class ConfigResolver5Transition(TestBase):
         Version 5 will accept a new "handler" argument. This should be
         accepted, but ignored in version 4.
         '''
-        with patch('config_resolver.Config') as mck:
+        with patch('config_resolver.core.Config') as mck:
             get_config('foo', 'bar', {}, handler='dummy_handler')
             get_config('foo', 'bar', {}, 'dummy_handler')
         mck.assert_called_with('bar', 'foo', filename='config.ini',
@@ -530,7 +530,7 @@ class ConfigResolver5Transition(TestBase):
         '''
         ``search_path`` should be taken from ``lookup_options``
         '''
-        with patch('config_resolver.Config') as mck:
+        with patch('config_resolver.core.Config') as mck:
             get_config('world', 'hello', lookup_options={
                 'search_path': 'testdata:testdata/a:testdata/b'
             })
@@ -543,7 +543,7 @@ class ConfigResolver5Transition(TestBase):
         '''
         ``filename`` should be taken from ``lookup_options``
         '''
-        with patch('config_resolver.Config') as mck:
+        with patch('config_resolver.core.Config') as mck:
             cfg_b = get_config('world', 'hello', lookup_options={
                 'filename': 'test.ini'
             })
@@ -557,7 +557,7 @@ class ConfigResolver5Transition(TestBase):
 
         We want the transition-layer to continue working as usual
         '''
-        with patch('config_resolver.Config') as mck:
+        with patch('config_resolver.core.Config') as mck:
             cfg_b = get_config('world', 'hello')
         mck.assert_called_with('hello', 'world',
             filename='config.ini',

config_resolver/core.py
@@ -18,6 +18,8 @@ from os import getcwd, getenv, pathsep
 from os.path import abspath, exists, expanduser, join
 from warnings import warn
 
+from .exc import IncompatibleVersion, NoVersionError
+
 __version__ = '4.2.4'
 
 
@@ -57,22 +59,6 @@ class PrefixFilter(object):
         # pylint: disable = missing-docstring
         record.msg = self._separator.join([self._prefix, record.msg])
         return True
-
-
-class IncompatibleVersion(Exception):
-    """
-    This exception is raised if a config file is loaded which has a different
-    major version number than expected by the application.
-    """
-    pass
-
-
-class NoVersionError(Exception):
-    """
-    This exception is raised if the application expects a version number to be
-    present in the config file but does not find one.
-    """
-    pass
 
 
 if sys.hexversion < 0x030000F0:
@@ -185,7 +171,7 @@ class Config(ConfigResolverBase):  # pylint: disable = too-many-ancestors
 
         # --- end of deprecation check --------------------------------------
 
-        self._log = logging.getLogger('{}.{}.{}'.format(__name__,
+        self._log = logging.getLogger('{}.{}.{}'.format('config_resolver',
                                                         group_name,
                                                         app_name))
         self._prefix_filter = PrefixFilter('group={}:app={}'.format(

config_resolver/exc.py
@@ -1,0 +1,12 @@
+class IncompatibleVersion(Exception):
+    """
+    This exception is raised if a config file is loaded which has a different
+    major version number than expected by the application.
+    """
+
+
+class NoVersionError(Exception):
+    """
+    This exception is raised if the application expects a version number to be
+    present in the config file but does not find one.
+    """

commit 0e33158afa572b44a7d43d25f2f209aefeb43aa3

Fix regression introduced by 54168cd

config_resolver/__init__.py
@@ -1,3 +1,4 @@
 # pylint: skip-file
-from .core import Config, SecuredConfig, __version__, get_config
+from .core import (Config, NoOptionError, NoSectionError, SecuredConfig,
+                   __version__, get_config)
 from .exc import NoVersionError

test.py
@@ -8,12 +8,8 @@ from contextlib import contextmanager
 from os.path import abspath, expanduser, join
 from textwrap import dedent
 
-from config_resolver import Config, NoVersionError, SecuredConfig, get_config
-
-try:
-    from ConfigParser import NoOptionError, NoSectionError
-except ImportError:
-    from configparser import NoOptionError, NoSectionError
+from config_resolver import (Config, NoOptionError, NoSectionError,
+                             NoVersionError, SecuredConfig, get_config)
 
 try:
     from mock import patch