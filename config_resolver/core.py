bb7d12c6eb13a7509b5d0477f8b1b8ebfdc26d1d
Michel Albert <michel@albert.lu>
2018-12-31 10:20:59 +0100

    Version bumped to 4.3.1.post1

--- config_resolver/core.py
+++ config_resolver/core.py
@@ -23,7 +23,7 @@ from warnings import warn
 from .exc import NoVersionError
 from .util import PrefixFilter
 
-__version__ = '4.3.1'
+__version__ = '4.3.1.post1'
 
 
 ConfigID = namedtuple('ConfigID', 'group app')
