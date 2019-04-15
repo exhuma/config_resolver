e24597754b56c68e1a10a32f7993f7af9e432c4b
Michel Albert <michel@albert.lu>
2019-04-15 08:59:01 +0200

    Version bumped to 4.3.4

--- config_resolver/core.py
+++ config_resolver/core.py
@@ -27,7 +27,7 @@ from warnings import warn
 from .exc import NoVersionError
 from .util import PrefixFilter
 
-__version__ = '4.3.3'
+__version__ = '4.3.4'
 
 
 ConfigID = namedtuple('ConfigID', 'group app')
